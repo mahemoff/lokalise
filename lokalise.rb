f83c3cbfcc3ad53e1318981059af8f89c6563fd9
Mahemoff <michael@mahemoff.com>
2015-07-02 19:37:54 +0100

    add "strip" functionality
    This required introducing official zip util instead of relying on command-line zip excutable, so we can determine which files are output

--- lokalise.rb
+++ lokalise.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#
+
 # CODE USAGE
 #   Player::Lokalise.new(options).download project_id
 #   See "property" declarations below for the available options
@@ -11,7 +11,7 @@
 #
 # (This should become a gem later on)
 
-%w(rubygems excon hashie json fileutils slop byebug).each { |gem| require gem }
+%w(rubygems excon hashie json fileutils zip slop byebug).each { |gem| require gem }
 
 ######################################################################
 # USING FROM COMMAND LINE
@@ -30,6 +30,7 @@ module Player
     property :output_folder
     property :output_format, default: :yml
     property :allow_overwrite
+    property :strip
 
     def initialize(options)
       super(options)
@@ -38,19 +39,26 @@ module Player
     end
 
     def download(project_id)
+      # save
       request_zip_from_lokalize project_id
       download_zip
       unzip
+      # post-process
+      do_strip
     end
 
     private
+
+    ######################################################################
+    # SAVE
+    ######################################################################
     
     def request_zip_from_lokalize(project_id)
       headers = { "Content-Type" => "application/x-www-form-urlencoded" }
       body = URI.encode_www_form(
           id: project_id,
           api_token: lokalise_api_token,
-          export_all: 1,
+          export_all: 0,
           type: self.output_format,
           use_original: '0',
           filter: 'translated',
@@ -83,15 +91,42 @@ module Player
         FileUtils.mkdir self.output_folder if !File.directory?(self.output_folder)
         Dir.chdir self.output_folder
       end
-      args = ['-qq']
-      args << '-o' if self.allow_overwrite
-      system "unzip #{args.join ' '} #{ZIP_FILE}"
-      log "Zip file has been extracted"
+      #args = ['-qq']
+      #args << '-o' if self.allow_overwrite
+      #system "unzip #{args.join ' '} #{ZIP_FILE}"
+      #log "Zip file has been extracted"
+      @output_files = []
+      Zip::File.open(ZIP_FILE) do |file|
+        file.each do |entry|
+          if entry.name!='./'
+            FileUtils.rm(entry.name) if self.allow_overwrite && File.exists?(entry.name)
+            entry.extract("#{entry.name}")
+            @output_files << entry.name
+          end
+        end
+      end
+    end
+
+    ######################################################################
+    # POST-PROCESS
+    ######################################################################
+
+    def do_strip
+      if self.strip
+        @output_files.each { |output_file|
+          find_and_replace output_file, /^.+""$\n/, ''
+        }
+      end
     end
 
     ######################################################################
     # HELPERS
     ######################################################################
+
+    def find_and_replace(file, pattern, replacement)
+      content = File.read(file).gsub(pattern, replacement)
+      File.open(file, "w") { |file| file.puts content }
+    end
     
     def log(s)
       puts s
@@ -118,6 +153,7 @@ END
     o.string '-t', '--token', 'API token (default: LOKALISE_API_TOKEN env variable)'
     o.string '-f', '--format', 'output format (default: yml)', default: :yml
     o.string '-o', '--output-folder', 'output folder (default: current folder; will be created if doesn''t exist)'
+    o.boolean '-s', '--strip', 'strip out entries with empty string value'
     o.on '-h', '--help', 'help' do ; puts opts && exit ; end
   end
   ARGV.replace opts.arguments
@@ -126,6 +162,7 @@ END
       lokalise_api_token: opts[:token],
       output_format: opts[:format],
       output_folder: opts['output-folder'],
+      strip: opts['strip'],
       allow_overwrite: true
     ).download project_id
   else
